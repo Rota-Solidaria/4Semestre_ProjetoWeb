730b24c7d92012c0f9d7f7e454278ff2ec6f8d97
Ramos902 <joaopedroramos1512@gmail.com>
2026-09-26 12:07:42 -0300

    feat: implement CampaignController and CampaignService for campaign management

---
+++ src/main/java/com/rotasolidaria/rotasolidaria/controllers/CampaignController.java
@@ -0,0 +1,41 @@
+// CampanhaController.java
+package com.rotasolidaria.rotasolidaria.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.rotasolidaria.rotasolidaria.models.Campaign;
+import com.rotasolidaria.services.CampaignService;
+
+@Controller
+public class CampaignController {
+
+    private final CampaignService campanhaService;
+
+    public CampaignController(CampaignService campanhaService) {
+        this.campanhaService = campanhaService;
+    }
+
+    @GetMapping("/campanhas")
+    public String listar(Model model) {
+        List<Campaign> campanhas = campanhaService.listarTodas();
+        model.addAttribute("campanhas", campanhas);
+        return "campanhas"; // -> templates/campanhas.ftlh
+    }
+
+    @GetMapping("/campanhas/{id}")
+    public String detalhes(@PathVariable Long id,
+            Model model) {
+        Optional<Campaign> campanha = campanhaService.buscarPorId(id);
+        if (campanha.isPresent()) {
+            model.addAttribute("campanha", campanha.get());
+            return "detalhes-campanha"; // -> templates/detalhes-campanha.ftlh
+        }
+        return "redirect:/campanhas";
+    }
+}
---
+++ src/main/java/com/rotasolidaria/services/CampaignService.java
@@ -0,0 +1,26 @@
+package com.rotasolidaria.services;
+
+import com.rotasolidaria.rotasolidaria.models.Campaign;
+import com.rotasolidaria.rotasolidaria.repositories.CampanhaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CampaignService {
+
+    private final CampanhaRepository campanhaRepository;
+
+    public CampaignService(CampanhaRepository campanhaRepository) {
+        this.campanhaRepository = campanhaRepository;
+    }
+
+    public List<Campaign> listarTodas() {
+        return campanhaRepository.findAll();
+    }
+
+    public Optional<Campaign> buscarPorId(Long id) {
+        return campanhaRepository.findById(id);
+    }
+}
